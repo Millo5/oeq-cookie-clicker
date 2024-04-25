e51173b961a3232ac180cf9a2677a1577606468a
Millo <23leeuw@gmail.com>
2024-04-25 10:04:38 +0200

    renamed methods

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from example.Python.WaterRowerConnector import WaterRowerConnector
 from example.Python.WaterRowerConnection import WaterRowerConnection
 
-def onEvent():
+
+def on_event():
     print("on")
 
-def onDisconnect():
+
+def on_disconnect():
     print("disconnect")
 
 
 def main():
-    connection = WaterRowerConnection(None, onEvent, onDisconnect)
+    connection = WaterRowerConnection(None, on_event, on_disconnect)
     water_rower = WaterRowerConnector(connection)
 
     water_rower.connect()
