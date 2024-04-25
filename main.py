e38f24102927ed7449d786d3b09406186a0fd1f2
Mklok <554021@student.saxion.nl>
2024-04-25 14:59:45 +0200

    Added full coverage of data to the file

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from WaterRowerConnection import WaterRowerConnection
 # Time
 # Distance
 
+info_dict = {}
+
 def lerp(start, end, t):
     return start * (1 - t) + end * t
 
@@ -19,6 +21,7 @@ class Example:
         self.connection = None
         self.pulses = 0
         self.delta = 0
+        info_dict["total_distance_m"] = info_dict["heart_rate"] = info_dict['total_strokes'] = info_dict['watts'] = info_dict['total_kcal'] = info_dict['avg_distance_cmps'] = info_dict['total_speed_cmps'] = info_dict['display_sec_dec'] = info_dict['display_sec'] = info_dict['display_min'] = info_dict['display_hr'] = info_dict['500mps'] = info_dict['stroke_rate'] = info_dict['avg_time_stroke_whole'] = info_dict['avg_time_stroke_pull'] = info_dict['tank_volume'] = 0
 
     def onDisconnect(self):
         self.connection = None
@@ -31,16 +34,34 @@ class Example:
         # run a GUI, do a full-screen blessed virtualization, or just about anything else.
         while self.connection:
             # print("Do awesome stuff here! Total pulses:", self.pulses)
+
             info_storage = open("WaterRowerDataPython.txt", 'w')
             self.delta = lerp(self.delta, self.pulses, 0.5)
             print()
             file_content = "delta:" + str(round(self.delta))
+            for k, v in info_dict.items():
+                file_content += "\n" + str(k) + ":" + str(v)
+
             info_storage.write(file_content + "\n")
             info_storage.close()
             print(" "*round(self.delta)+"-")
             self.pulses = 0
             self.connection.requestStatistic("total_distance_m")
             self.connection.requestStatistic("heart_rate")
+            self.connection.requestStatistic("total_strokes")
+            self.connection.requestStatistic("watts")
+            self.connection.requestStatistic("total_kcal")
+            self.connection.requestStatistic("avg_distance_cmps")
+            self.connection.requestStatistic("total_speed_cmps")
+            self.connection.requestStatistic("display_sec_dec")
+            self.connection.requestStatistic("display_sec")
+            self.connection.requestStatistic("display_min")
+            self.connection.requestStatistic("display_hr")
+            self.connection.requestStatistic("500mps")
+            self.connection.requestStatistic("stroke_rate")
+            self.connection.requestStatistic("avg_time_stroke_whole")
+            self.connection.requestStatistic("avg_time_stroke_pull")
+            self.connection.requestStatistic("tank_volume")
             time.sleep(0.05)
 
     def onEvent(self, event):
@@ -48,8 +69,7 @@ class Example:
         if event["type"] == "pulse":
             self.pulses += event["value"]
         else:
-            pass
-            # print('event', event, flush=True)
+            info_dict[event['type']] = event['value']
 
 
     def connect(self):
