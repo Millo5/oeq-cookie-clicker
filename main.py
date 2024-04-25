cd3457903c182860fb75cc5824561c5433684ed1
Millo <23leeuw@gmail.com>
2024-04-25 10:03:37 +0200

    Second

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# this is a test upload
+from example.Python.WaterRowerConnector import WaterRowerConnector
+from example.Python.WaterRowerConnection import WaterRowerConnection
+
+def onEvent():
+    print("on")
+
+def onDisconnect():
+    print("disconnect")
+
+
+def main():
+    connection = WaterRowerConnection(None, onEvent, onDisconnect)
+    water_rower = WaterRowerConnector(connection)
+
+    water_rower.connect()
+
+
+if __name__ == "__main__":
+    main()
